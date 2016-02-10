1d201b5d6b215f3c65e5877f377cdb7e7e67355a
Abhijith Ravuri <KH9126@kitspl.com>
2016-02-10 19:43:15 +0530

    Changed removeDuplciates returned int

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -2,18 +2,20 @@
 OVERVIEW:  given an array that has duplicate values remove all duplicate values.
 E.g.: input: [1, 1, 2, 2, 3, 3], output [1, 2, 3]
 
-INPUTS: Integer array and length of the array.
+Ex 2: [4,4,2,2,1,5] output : [4,2,1,5]
 
-OUTPUT: Update input array by removing duplicate values.
+INPUTS: Integer array and length of the array .
 
-ERROR CASES: Return NULL for invalid inputs.
+OUTPUT: Update input array by removing duplicate values and return the final array length
+
+ERROR CASES: Return -1 for invalid inputs.
 
 NOTES: Don't create new array, try to change the input array.
 */
 
-#include <stdio.h>
 
-void * removeArrayDuplicates(int *Arr, int len)
+int removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	
+	return -1;
 }
