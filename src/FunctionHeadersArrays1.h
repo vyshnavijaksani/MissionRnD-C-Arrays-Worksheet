1912070243ce145b964dc9543ff7ad8e523b9df8
Abhijith Ravuri <KH9126@kitspl.com>
2016-02-15 14:28:02 +0530

    AddedHeaderFiles

---
+++ src/FunctionHeadersArrays1.h
@@ -0,0 +1,6 @@
+int removeArrayDuplicates(int *, int);
+void * scoresDescendingSort(struct student *,int );
+void * studentsCount(int *, int, int, int *, int *);
+int * sortedArrayInsertNumber(int *, int, int);
+void * sortedArrayPositionsChange(int *, int);
+struct student ** topKStudents(struct student *,int ,int);
---
+++ src/MainArrays1.cpp
@@ -0,0 +1,22 @@
+#include "FunctionHeadersArrays1.h"
+#include <stdio.h>
+
+int main(){
+
+	//Test RemoveArraysDuplicates
+	/*
+	int arr[3] = { 1, 2, 7 };
+	int len = removeArrayDuplicates(arr, 3);
+	printf("%d", len);
+	*/
+
+	//Test Students Count
+	/*
+	int arr2[3]={1,4,10};
+	int *newarray=sortedArrayInsertNumber(arr, 3, 3);
+	printf("%d %d %d %d", newarray[0], newarray[1], newarray[2],newarray[3]);
+	*/
+
+
+	return 0;
+}
